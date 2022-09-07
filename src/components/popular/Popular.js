4a141770baf9476da5a0cc03b4ecc55063b83655
Denis <denismunene999@gmail.com>
2022-09-07 17:07:21 +0300

    create add new book button

--- src/components/popular/Popular.js
+++ src/components/popular/Popular.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import './Popular.css'
 
 function Popular() {
+
+  const[newBookForm, setNewBookForm] = useState(false)
+
+  function handleClick() {
+    setNewBookForm(!newBookForm)
+  }
+
   return (
-    <div>Popular</div>
+    <div>
+      <button className='post-book' onClick={handleClick}> {newBookForm ? 'Hide form': 'Add a book'} </button>
+    </div>
   )
 }
 
