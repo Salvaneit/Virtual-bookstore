44702d3a7d457167e2ed5d38eec2a8256e3afb51
Denis <denismunene999@gmail.com>
2022-09-07 16:33:50 +0300

    complete home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,13 +3,29 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='text-container'>
-        <h1>Welcome to<br/> Books Bay bookstore</h1>
-        <p className='grey'>With us you can shop online<br/> and save on high street costs<br/> at the same time.</p>
-        <button className='explore'>Explore books &#8594;</button>
+    <div>
+      <div className='home'>
+        <div className='text-container'>
+          <h1>Welcome to<br/> Books Bay bookstore</h1>
+          <p className='grey'>With us you can shop online<br/> and save on high street costs<br/> at the same time.</p>
+          <button className='explore'>Explore books &#8594;</button>
+        </div>
+        <img className='home-img' src='https://cdn.britannica.com/92/216092-050-4B31C2B7/custom-library.jpg' alt='store' />
       </div>
-      <img className='home-img' src='https://cdn.britannica.com/92/216092-050-4B31C2B7/custom-library.jpg' alt='store' />
+      <div className='footer'>
+        <div>
+          <h3>Shipping</h3>
+          <p className='grey'>Country wide delivery</p>
+        </div>
+        <div>
+          <h3>Best price</h3>
+          <p className='grey'>Best price with best <br/> quality</p>
+        </div>
+        <div>
+          <h3>Authors</h3>
+          <p className='grey'>Best Kenyan Novelists</p>
+        </div>
+      </div>
     </div>
   )
 }
