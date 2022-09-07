fbf54b795027e2708df985ca5b26f6841560c411
Denis <denismunene999@gmail.com>
2022-09-07 15:34:46 +0300

    create home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='text-container'>
+        <h1>Welcome to<br/> Books Bay bookstore</h1>
+        <p>With us you can shop online<br/> and save on high street costs<br/> at the same time.</p>
+        <button>Explore books</button>
+      </div>
+      <img className='' src='https://cdn.britannica.com/92/216092-050-4B31C2B7/custom-library.jpg' alt='store' />
+    </div>
   )
 }
 
