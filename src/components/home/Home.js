320794552a284c590caeb984ad10741c11db387a
Denis <denismunene999@gmail.com>
2022-09-07 16:01:13 +0300

    style home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,10 +6,10 @@ function Home() {
     <div className='home'>
       <div className='text-container'>
         <h1>Welcome to<br/> Books Bay bookstore</h1>
-        <p>With us you can shop online<br/> and save on high street costs<br/> at the same time.</p>
-        <button>Explore books</button>
+        <p className='grey'>With us you can shop online<br/> and save on high street costs<br/> at the same time.</p>
+        <button className='explore'>Explore books &#8594;</button>
       </div>
-      <img className='' src='https://cdn.britannica.com/92/216092-050-4B31C2B7/custom-library.jpg' alt='store' />
+      <img className='home-img' src='https://cdn.britannica.com/92/216092-050-4B31C2B7/custom-library.jpg' alt='store' />
     </div>
   )
 }
