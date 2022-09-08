93d0ffe36882c1ed9c7d9e8fb0e3595f1bc13ae9
Denis <denismunene999@gmail.com>
2022-09-08 20:25:03 +0300

    style author cards

--- src/components/bibliography/Author.js
+++ src/components/bibliography/Author.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Author.css'
 
-function Author() {
+function Author({author}) {
+
+  const{id, name, about, books} = author
+
+  const booksArray = books.map((book) => {
+    return book.name
+  })
+
   return (
-    <div>Author</div>
+    <div className='author-card' >
+      <div className='content'>
+        <h3>{name}</h3>
+        <p>{about}</p>
+        <p>Books: {booksArray.join(", ")}</p>
+        <p>Author id: {id}</p>
+      </div>
+    </div>
   )
 }
 
